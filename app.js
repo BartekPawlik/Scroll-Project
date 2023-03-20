4998e6b5e6445b0dc7afa1fbe829d93ae3631c3a
Bartek Pawlik <baxon.bobson@gmail.com>
2023-03-20 21:33:33 +0100

    add to JS getBoundingClientRect

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ date.innerHTML = new Date().getFullYear()
 
 const navToogle = document.querySelector(".nav-toogle");
 const linksContainer = document.querySelector('.links-container');
-const link = document = document.querySelectorAll('.scroll-link');
+const link = document.querySelector('.links');
 
-navToogle.addEventListener('click', function(){
-    linksContainer.classList.toggle('show-links')
+navToogle.addEventListener("click", function(){
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linkHeight = link.getBoundingClientRect().height;
+  
+    // console.log(linkHeight)
+
+    if (containerHeight === 0) {
+        linksContainer.style.height = `${linkHeight}px`;
+
+    }else{
+        linksContainer.style.height = 0;
+    }
+  
 })
