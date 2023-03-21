2dd1d6aa41eed3dbbdeaebee5e86270c82bede67
Bartek Pawlik <baxon.bobson@gmail.com>
2023-03-21 18:29:22 +0100

    add fixed nav

--- app.js
+++ app.js
@@ -19,3 +19,47 @@ navToogle.addEventListener("click", function(){
     }
   
 })
+
+
+
+
+const navbar = document.getElementById('nav')
+window.addEventListener("scroll", function(){
+   const scrollHeihgt = window.pageYOffset;
+   const navHeight = navbar.getBoundingClientRect().height;
+   if(scrollHeihgt > navHeight){
+    navbar.classList.add('fixed-nav')
+   }else{
+    navbar.classList.remove('fixed-nav')
+   }
+})
+
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(function(link){
+link.addEventListener('click', function(e){
+e.preventDefault();
+const id = e.currentTarget.getAttribute ('href').slice(1);
+const element = document.getElementById(id);
+// calculate the heights
+const navHeight = navbar.getBoundingClientRect().height;
+const containerHeight = linksContainer.getBoundingClientRect().height
+const fixedNav = navbar.classList.contains('fixed-nav');
+
+let position = element.offsetTop - navHeight - 5;
+
+if (!fixedNav){
+    position = position - navHeight
+}
+if(navHeight > 80) {
+    position = position + containerHeight;
+}
+
+window.scrollTo({
+    left: 0,
+    top: position,})
+
+
+});
+linksContainer.style.height = 0
+});
